47016090bf82e9f7df5eeea86e6af94abf85b74c
Harro Lissenberg <hlissenberg@qualogy.com>
2017-11-20 09:19:10 +0100

    Initital commit

---
+++ hello-server/src/main/java/org/hello/server/rest/HelloWorldEndpoint.java
@@ -0,0 +1,27 @@
+package org.hello.server.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+	@GET
+	@Produces("text/plain")
+	public Response doGet() {
+		return Response.ok("Hello from WildFly Swarm! The time is: " + getTime()).build();
+	}
+
+
+	private String getTime() {
+		return ClientBuilder.newClient()
+				.target("http://time-server:8080/time")
+				.request()
+				.get(String.class);
+	}
+}
